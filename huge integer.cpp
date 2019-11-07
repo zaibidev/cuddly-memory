2f1e3103d0dcd79b082cae64b9e4bea788b9b51f
Muhammad Sarim Hassan <51859017+Sarim5@users.noreply.github.com>
2019-11-07 22:44:13 +0500

    Add files via upload
    User can enter a integer that is 30 number long

---
+++ huge integer.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+class hugeinteger
+{
+	string no;
+	int strl, sum[30], subt[30], nos[30], tempsum, tempsub;
+	public:
+	void input (void)
+	{
+		cout<<"ENTER A NUMBER"<<endl;
+		cin>>no;
+	}
+	void strlength(void)
+	{
+		strl=no.length();
+	}
+	void covertintono(void)
+	{
+		for (int i=0;i<strl;i++)
+		{
+			nos[i]=no[i]-48;
+		}
+	}
+	 void addition(hugeinteger obj)
+	{
+		//tempsum=0;
+		for (int i=strl-1;i>=0;i--)
+		{
+			tempsum=nos[i]+obj.nos[i];
+			if(tempsum>9&&i>0)
+			{
+				sum[i]=tempsum-10;
+				nos[i-1]=nos[i-1]+1;
+				
+			}
+			else
+			{
+				sum[i]=tempsum;
+			}
+			tempsum=0; 
+		}
+	}
+	void display()
+	{
+		cout<<"SUM IS"<<endl;
+		for (int i=0;i<strl;i++)
+		{
+			cout<<sum[i];
+		}
+	}
+};
+int main()
+{
+	hugeinteger obj1;
+	hugeinteger obj2;
+	obj1.input();
+	obj2.input();
+	obj1.strlength();
+	obj2.strlength();
+	obj1.covertintono();
+	obj2.covertintono();
+	obj2.addition(obj1);
+	obj2.display();
+}
